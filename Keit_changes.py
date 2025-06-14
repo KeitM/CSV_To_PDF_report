b0fb7f6186174823c14901240b5cee773e2fad93
katmunkova <katmunkova@gmail.com>
2025-06-14 17:49:12 +0300

    поиграла с бибилотеками

--- Keit_changes.py
+++ Keit_changes.py
@@ -3,6 +3,7 @@ import pandas as pd
 import polars as pl
 import csv
 
+from pandas import DataFrame  as df
 
 from datetime import datetime
 
@@ -14,12 +15,49 @@ import matplotlib.pyplot as plt # for data visualization
 
 chunk_size = 10000
 
+# # Чтение файла по частям
+# chunks = pd.read_csv('btcusd_1-min_data.csv', chunksize=chunk_size)
+
+# with pd.read_csv('btcusd_1-min_data.csv', chunksize=chunk_size) as reader:
+#     for chunk in reader:
+#         with open('btcusd_1-min_data.csv', 'r') as file:
+#             reader = csv.reader(file)
+#            # df = pd.DataFrame(reader)
+#            # next(reader) 
+#             for row in reader:
+#                  for column in reader:
+#                 #     chunk['timestamp'].head()
+#                 #     df['date'] = pd.to_datetime(df['timestamp_ms'], unit='ms')  
+#                 #  #   reader['timestamp'] = pd.Timestamp.to_datetime(train_full['timestamp'])
+#                     print(row)
+                    
+
+
 # Чтение файла по частям
-chunks = pd.read_csv('btcusd_1-min_data.csv', chunksize=chunk_size)
+
+
+averages = []
 
 with pd.read_csv('btcusd_1-min_data.csv', chunksize=chunk_size) as reader:
-    for chunk in reader:
-        with open('btcusd_1-min_data.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row)
+    for i, chunk in enumerate(reader):
+        print(f"Обработка порции {i + 1}...")
+
+       
+        if 'Timestamp' in chunk.columns:
+            chunk['date'] = pd.to_datetime(chunk['Timestamp'], unit='ms')
+      
+        else:
+            print("Нет подходящего столбца с timestamp")
+            continue
+        
+        
+        if 'Open' in chunk.columns:
+            avg_Open = chunk['Open'].mean()
+            averages.append({'chunk': i+1, 'avg_Open': avg_Open})
+        else:
+            averages.append({'chunk': i+1, 'avg_Open': None})
+        
+        
+        # Теперь можешь работать с chunk, например:
+     #   print(chunk[['date']].head())  # Вывести первые строки текущей порции
+        print(averages)
